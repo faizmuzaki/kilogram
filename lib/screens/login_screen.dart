12c6343d6dfcc10599859771b67fc58bb05ef03f
Faiz Muzaki <34159640+faizmuzaki@users.noreply.github.com>
2023-10-16 15:42:52 +0700

    initial commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Loginscreen extends StatelessWidget {
+  const Loginscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text("Coba"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
